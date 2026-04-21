e78fb4caf41e5e07f27fe8d88712c804fbcd7c22
Mangilipelly saikumar <mangilipellysai.11@gmail.com>
2026-04-21 18:52:14 +0530

    Installations

---
+++ new-installations.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error :: please run with root user"
+    exit 1
+else 
+    echo "You are running with root access"
+fi 
+
+VALIDATION() {
+    if [ $1 -eq 0 ]
+    then 
+        echo "you are going to install $2 ...."
+    else 
+        echo "$2 is already installed..."
+}
+
+dnf list installed mysql
+if [ $? -ne 0]
+then 
+    echo "Installing mysql..."
+    dnf install mysql -y
+    VALIDATION $? "mysql"
+else
+    echo "mysql is already installed..."
+fi 
+
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "Installing python3..."
+    dnf install python3 -y
+    VALIDATION $? "python3"
+else 
+    echo "python3 is already installed..."
+fi
+
+
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "Installing nginx..."
+    dnf install nginx -y
+    VALIDATION $? "nginx"
+else 
+    echo "nginx is already installed..."
+fi
